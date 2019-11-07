c0fe4ffaea781f2fe6be4e2dcebbfac29266e6e9
zaimsyaddad99 <zaimibnusyam@gmail.com>
2019-11-07 10:18:40 -0800

    asd

--- main_front.py
+++ main_front.py
@@ -12,7 +12,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
+        MainWindow.setObjectName("FaceRecognition")
         MainWindow.resize(869, 638)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
